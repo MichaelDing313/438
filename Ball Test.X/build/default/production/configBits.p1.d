7407982bbb8a068fb3beab7f0013ab6e42b6f1c5
MichaelDing313 <Michael.Ding313@gmail.com>
2017-03-19 16:10:56 -0400

    First Commit, Starting Files 18F45K40
    Starter file, this is for the
    PIC18F45K40

---
+++ Ball Test.X/build/default/production/configBits.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/configBits.d  \
+ build/default/production/configBits.p1:  \
+ configBits.c 
---
+++ Ball Test.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Michael
+ *
+ * Created on March 19, 2017, 1:00 PM
+ */
+
+#include "configBits.c"
+#include <xc.h>
+
+void main(void) {
+    return;
+}
